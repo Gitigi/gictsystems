1f70ba0f6cdca94869eb4cbe566e2429968d3a14
gitigi <stephengitigi@gmail.com>
2022-09-22 12:04:15 +0300

    feat: added title

--- components/layout.js
+++ components/layout.js
@@ -1,14 +1,20 @@
+import Head from "next/head"
 import Header from "./header"
 import Footer from "./footer"
 
 export default function Layout({ active, children }) {
   return (
-    <div class="flex flex-col h-screen">
-        <Header active={active} />
-        <main>
-          {children}
-        </main>
-        <Footer />
-    </div>
+    <>
+      <Head>
+        <title>Gictsystems</title>
+      </Head>
+      <div class="flex flex-col h-screen">
+          <Header active={active} />
+          <main>
+            {children}
+          </main>
+          <Footer />
+      </div>
+    </>
   )
 }
